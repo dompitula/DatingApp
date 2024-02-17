7f88e93c16b3c846a743b7e1bf20716eb5ced235
dompitula <pituladom@gmail.com>
2024-02-17 02:29:25 +0100

    added migrations

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bca021cda705ab6a5fe547ba452b40360861eb10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab5b39b1b07b10b688ecf29ef8b1062301be7b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
